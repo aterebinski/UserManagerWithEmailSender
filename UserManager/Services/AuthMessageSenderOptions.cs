b8a8d0e253a21866606ec8f9f54c5733f8cd6545
Adam Terebiński <aterebinski@wp.pl>
2021-12-14 16:21:22 +0100

    Install SendGrid. Follow: https://docs.microsoft.com/en-us/aspnet/core/security/authentication/accconfirm?view=aspnetcore-6.0&tabs=visual-studio

---
+++ UserManager/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManager.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridKey { get; set; }
+    }
+}
